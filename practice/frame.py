7ea4728f841b769e4083a146dfa2e8e87617649e
ImCaos <mrcaos@foxmail.com>
2015-04-28 21:29:31 +0800

    项目规划脑图文件和Tkinter练习类

---
+++ practice/frame.py
@@ -0,0 +1,13 @@
+__author__ = 'caoshuai'
+
+from Tkinter import *
+
+master = Tk()
+
+Label(text="one").pack()
+separator = Frame(height=2, bd=1, relief=SUNKEN)
+separator.pack(fill=X, padx=5, pady=5)
+
+Label(text="two").pack()
+
+mainloop()
---
+++ practice/hello1.py
@@ -0,0 +1,11 @@
+__author__ = 'Caos'
+
+from Tkinter import *
+
+root = Tk()
+
+w = Label(root, text='Hello world!')
+w.pack()
+
+root.mainloop()
+
