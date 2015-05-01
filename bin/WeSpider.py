9f2866c2992aa48afd0985c7f6c15de43199dc00
ImCaos <mrcaos@foxmail.com>
2015-05-01 21:57:48 +0800

    抓取链接校验规则

--- bin/WeSpider.py
+++ bin/WeSpider.py
@@ -9,10 +9,18 @@ from tkFileDialog import *
 
 from Tkinter import *
 
+from tkMessageBox import *
+
 import re
 
+# 显示信息提示框
+def show_messagebox(icon, title, message):
+    Message().show(icon=icon, title=title, message=message)
 
-_RE_URL = r'http://weibo.com/'
+
+_RE_URL = r'^http://weibo.com/\S.'
+_RE_URL_WITH_ID = r'^http://weibo.com/u/\d{10}'
+_RE_URL_WITH_NAME = r'http://weibo.com/\w{3,}'
 
 # 链接面板
 class LinkPanel:
@@ -35,11 +43,11 @@ class LinkPanel:
 
         self.textArea.pack()
 
-        self.cleanBtn = Button(linkPanel, text="清除链接", padx=50, pady=5, command=self.clean_text_value)
+        self.cleanBtn = Button(linkPanel, text="清除链接", padx=50, pady=15, command=self.clean_text_value)
 
         self.cleanBtn.pack(side=LEFT, padx=50)
 
-        self.checkBtn = Button(linkPanel, text="校验链接", padx=50, pady=5, command=self.check_text_value)
+        self.checkBtn = Button(linkPanel, text="校验链接", padx=50, pady=15, command=self.check_text_value)
 
         self.checkBtn.pack(side=RIGHT, padx=50)
 
@@ -47,12 +55,54 @@ class LinkPanel:
 
     # 检查输入的链接地址是否符合规范，包括排重，校验HTTP链接，格式化
     def check_text_value(self):
-        urls = self.textArea.get(0, END).strip()
 
+        urlStr = self.textArea.get(1.0, END)
+        if urlStr == u'\n':
+            show_messagebox(ERROR, '提示', '链接不允许为空')
+            # 动态设置字体颜色
+            # self.textArea.tag_add("red", "1.0", END)
+            # self.textArea.tag_config("red", background="white", foreground="red")
+            return
+        else:
+            # self.textArea.tag_add("red", "1.0", END)
+            # self.textArea.tag_config("red", background="white", foreground="black")
+            pass
+
+        rawUrls = urlStr.strip().replace('\n', '').split(';')
+        urls = []
+        for url in rawUrls:
+
+            # 带有路径u标识的链接，e: weibo.com/u/\d{10}
+            idFlag = re.match(_RE_URL_WITH_ID, url)
+            nameFlag = re.match(_RE_URL_WITH_NAME, url)
+            if idFlag:
+                url = idFlag.group()
+                urls.append(url+';')
+            elif nameFlag:
+                url = nameFlag.group()
+                urls.append(url+';')
+            else:
+                pass
+
+        if len(urls)==0:
+            show_messagebox(WARNING, '警告', '输入链接不符合规则')
+
+        else:
+            # 去重
+            urls = reduce(lambda x, y: x if y in x else x+[y], [[],]+urls)
+            # 格式化链接
+            self.textArea.delete(1.0, END)
+            # 保存抓取链接
+            self.urls = urls
+            # 设置checked字段
+            self.urlChecked = TRUE
+            self.textArea.insert(INSERT, '\n'.join(urls))
 
     # 清除文本框中的链接
     def clean_text_value(self):
         self.textArea.delete(0.0, END)
+        self.urlChecked = FALSE
+
 
 
 
@@ -112,6 +162,19 @@ class ConfigPanel:
 
 
 
+class LaunchPanel:
+    def __init__(self, master):
+        launchPanel = Frame(master, bd=2, width=300, padx=20, pady=0, bg=None)
+
+        launchPanel.pack()
+
+        self.launchButton = Button(launchPanel, command=self.check_all_config, text="开始抓取", padx=20, pady=20)
+
+        self.launchButton.pack()
+
+
+    def check_all_config(self):
+        pass
 
 # 初始化主应用面板
 mainPanel = Tk(className=" WeSpider ", baseName="WeSpider")
@@ -123,6 +186,6 @@ configPanel = ConfigPanel(mainPanel)
 
 filePanel = FileSelectPanel(mainPanel)
 
-
+launchPanel = LaunchPanel(mainPanel)
 
 mainPanel.mainloop()
