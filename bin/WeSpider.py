c53c1bf49ff385566125b8b5a6f188deba97bb0b
ImCaos <mrcaos@foxmail.com>
2015-05-02 22:30:34 +0800

    主面板校验规则完成

--- bin/WeSpider.py
+++ bin/WeSpider.py
@@ -32,7 +32,7 @@ class LinkPanel:
         # 链接面板渲染
         linkPanel.pack()
         # 内部组件的渲染
-        self.label = Label(linkPanel, text="输入抓取链接")
+        self.label = Label(linkPanel, text="输入抓取链接，多个链接用分号隔开")
 
         self.label.pack()
         # 设置文档的输入框
@@ -47,7 +47,7 @@ class LinkPanel:
 
         self.cleanBtn.pack(side=LEFT, padx=50)
 
-        self.checkBtn = Button(linkPanel, text="校验链接", padx=50, pady=15, command=self.check_text_value)
+        self.checkBtn = Button(linkPanel, text="格式化链接", padx=50, pady=15, command=self.check_text_value)
 
         self.checkBtn.pack(side=RIGHT, padx=50)
 
@@ -58,15 +58,10 @@ class LinkPanel:
 
         urlStr = self.textArea.get(1.0, END)
         if urlStr == u'\n':
-            show_messagebox(ERROR, '提示', '链接不允许为空')
-            # 动态设置字体颜色
-            # self.textArea.tag_add("red", "1.0", END)
-            # self.textArea.tag_config("red", background="white", foreground="red")
-            return
-        else:
-            # self.textArea.tag_add("red", "1.0", END)
-            # self.textArea.tag_config("red", background="white", foreground="black")
-            pass
+            # show_messagebox(ERROR, '提示', '链接面板渲染')
+
+            return False
+
 
         rawUrls = urlStr.strip().replace('\n', '').split(';')
         urls = []
@@ -85,7 +80,10 @@ class LinkPanel:
                 pass
 
         if len(urls)==0:
-            show_messagebox(WARNING, '警告', '输入链接不符合规则')
+            # show_messagebox(WARNING, '警告', '输入链接不符合规则')
+            self.textArea.tag_add("red", "1.0", '1.4')
+            self.textArea.tag_config("red", background="red", foreground="white")
+            return False
 
         else:
             # 去重
@@ -103,7 +101,14 @@ class LinkPanel:
         self.textArea.delete(0.0, END)
         self.urlChecked = FALSE
 
-
+    # 检查组件的校验状态
+    def check_status(self):
+        self.check_text_value()
+        if not self.urlChecked:
+            show_messagebox(WARNING, '警告', '输入链接不符合规则,请按照规则输入！')
+            return False
+        else:
+            return True
 
 
 # 选择文件保存位置的面板
@@ -115,7 +120,7 @@ class FileSelectPanel:
 
         exportPanel.pack()
 
-        self.fileEntry = Entry(exportPanel, width=50 )
+        self.fileEntry = Entry(exportPanel, width=50, validate="focusout", validatecommand=self.validate_dir)
 
         self.fileEntry.pack(side=LEFT, padx=0)
 
@@ -124,19 +129,42 @@ class FileSelectPanel:
 
         self.selectDirBtn.pack(side=RIGHT, padx=50)
 
+        self.dirChecked = False
+
+        self.filePath = ''
+
     # 路径选择面板
     def select_dir(self):
-        pass
+
         self.fileEntry.delete(0, END)
 
         self.filePath = askdirectory()
 
         if self.filePath:
             self.fileEntry.insert(0, self.filePath)
+            self.dirChecked = True
 
-    def check_dir(self):
-        pass
-        # TODO
+    def validate_dir(self):
+        if not self.filePath == self.fileEntry.get():
+            show_messagebox(ERROR, '错误', '存放路径不合法，请重新选择')
+            # 重置filePath的值
+            self.filePath = ''
+            self.fileEntry.delete(0, END)
+
+        else:
+            self.dirChecked = True
+
+     # 检查组件的校验状态
+    def check_status(self):
+
+        if not self.dirChecked:
+            show_messagebox(WARNING, '警告', '存储路径错误')
+            return False
+        elif self.filePath == '':
+            show_messagebox(ERROR, '警告', '为选择导出文件的存储路径')
+            return False
+        else:
+            return True
 
 # 抓取配置面板
 class ConfigPanel:
@@ -149,7 +177,7 @@ class ConfigPanel:
 
         self.imageCheckVar = IntVar()
         self.imageCheck = Checkbutton(configPanel, text = "抓取图片", variable = self.imageCheckVar, onvalue = 1, offvalue = 0, height=5, width = 10)
-        self.imageCheck.pack(side=RIGHT, padx=130)
+        self.imageCheck.pack(side=RIGHT, padx=110)
 
         numPanel = Frame(configPanel)
         numPanel.pack(side=LEFT, padx=70)
@@ -157,10 +185,31 @@ class ConfigPanel:
         self.numLabel = Label(numPanel, text="抓取数量")
         self.numLabel.pack(side=LEFT, padx=10)
 
-        self.numEntry = Entry(numPanel, width=10 )
+        self.numEntry = Entry(numPanel, width=20, validate="focusout", validatecommand=self.validate_num)
         self.numEntry.pack(side=LEFT, padx=0)
+        self.numChecked = True
+        self.fetchNum = 0
 
+    def validate_num(self):
+        self.fetchNum = self.numEntry.get()
+        # 非空且不为零的时候不通过校验
+        if not self.fetchNum.isdigit() and self.fetchNum is not '':
+            show_messagebox(WARNING, '警告', '请输入有效数字')
+            self.fetchNum = 0
+            self.numEntry.delete(0, END)
 
+        else:
+            # 通过校验后设置标识
+            self.numChecked = True
+
+    # 检查组件的校验状态
+    def check_status(self):
+
+        if not self.numChecked:
+            show_messagebox(WARNING, '警告', '抓取数量不符合规则')
+            return False
+        else:
+            return True
 
 class LaunchPanel:
     def __init__(self, master):
@@ -168,13 +217,26 @@ class LaunchPanel:
 
         launchPanel.pack()
 
-        self.launchButton = Button(launchPanel, command=self.check_all_config, text="开始抓取", padx=20, pady=20)
+        self.launchButton = Button(launchPanel, command=self.start_spider, text="开始抓取", padx=20, pady=20)
 
         self.launchButton.pack()
 
 
-    def check_all_config(self):
-        pass
+    def start_spider(self):
+        start()
+
+# 开始方法
+def start():
+    # 配置校验
+    if not linkPanel.check_status():
+        return False
+    if not configPanel.check_status():
+        return False
+    if not filePanel.check_status():
+        return False
+
+    # TODO
+
 
 # 初始化主应用面板
 mainPanel = Tk(className=" WeSpider ", baseName="WeSpider")
